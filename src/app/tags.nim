fb5e5fdc1a4ef1f6405d998fac92942dc00604c6
Rami Kader <ramy.kader.rk@gmail.com>
2024-02-21 13:32:16 +0100

    quiet list tags

--- src/app/tags.nim
+++ src/app/tags.nim
@@ -1,13 +1,22 @@
 from std/strutils import join
 
 
-proc listTags*(tagpattern: string, system: bool, quiet: bool) = echo "show " & (if system: "system" else: "non-system") & " tags."
-proc syncTags*(yes: bool) = echo "sync tags" & (if yes: " forcing updates" else: " suggestions") & "."
+proc listTags*(tagpattern: string, system: bool, quiet: bool) =
+  echo "show " & (if system: "system" else: "non-system") &
+    " tags" & (if quiet: " quitely" else: "") & "."
+
+proc syncTags*(yes: bool) =
+  echo "sync tags" &
+    (if yes: " applying updates" else: " suggestions") & "."
+
 proc tagFiles*(filepattern: string, tags: seq[string], hard: bool) =
-  echo "tag " & (if hard: "(hard copies of)" else: "") & filepattern & " by #" & $tags.join(" #")
+  echo "tag " & (if hard: "(hard copies of) " else: "") & filepattern & " by #" & $tags.join(" #")
+
 proc untagFiles*(filepattern: string, tags: seq[string]) =
   echo "untag " & filepattern & (if tags.len > 0: " by #" & $tags.join(" #") else: "")
+
 proc modTag*(name: string, newname: string) = echo "edit tag #" & name & " to " & newname
+
 proc removeTag*(tag: string, files: seq[string]) =
   if files.len == 0: echo "Removing tag " & tag
   else: echo "untag " & $files & " by #" & tag
