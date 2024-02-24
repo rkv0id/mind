bb94d42ed7d1737916b515ccfa1cd0707611e930
Rami Kader <ramy.kader.rk@gmail.com>
2024-02-24 23:40:13 +0100

    sync as a mode for find

--- src/mind.nim
+++ src/mind.nim
@@ -14,20 +14,19 @@ Usage:
   mind -h
   mind -v
   mind ls [-s] [-q] [<tagpattern>]
-  mind sync [-y]
   mind tag [-H] <filepattern> <tags>...
   mind untag <filepattern> [<tags>...]
   mind mv <name> <newname>
   mind desc <tag> <description>
   mind rm <tags>...
-  mind find [-t=<level>] <query>
+  mind find [-t=<level>] [-S] <query>
 
 Options:
   -h --help               Show this screen.
   -v --version            Show version.
   -s --system             Show only system tags.
   -q --quiet              Show a shorter more concise version of the output.
-  -y --yes                Silently auto-confirm synchronisation of tags without reporting loaded changes.
+  -S --sync               Synchronise query results before output.
   -H --hard               Create a hard link for a file and tag it.
   -t LEVEL --tree=LEVEL   Show results in tree mode until LEVEL then revert to list mode. [default: 0]
 """
@@ -36,7 +35,6 @@ when isMainModule:
   let args = docopt(doc, version = "MIND v0.1.0")
 
   args.dispatchProc(listTags, "ls")
-  args.dispatchProc(syncTags, "sync")
   args.dispatchProc(tagFiles, "tag")
   args.dispatchProc(untagFiles, "untag")
   args.dispatchProc(modTag, "mv")
