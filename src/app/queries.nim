f869680cf651ad98d25883cdb716df1e750270d0
Rami Kader <ramy.kader.rk@gmail.com>
2024-03-02 21:22:15 +0100

    feat - parse query to ast

--- src/app/queries.nim
+++ src/app/queries.nim
@@ -1,5 +1,6 @@
-from std/sequtils import mapIt, toSeq
-from regex import re2, findAll, group, match
+import std/deques
+from std/sequtils import mapIt
+from regex import re2, findAll
 
 type
   NodeKind = enum
@@ -12,42 +13,49 @@ type
     of nkAnd, nkOr: leftOp, rightOp: Node
 
 
-proc tokenize(input: string): seq[string] =
-  const matchRgx = re2"(?:s\/|#|\.)[a-zA-Z_]\w*|t\/[a-zA-Z]+|and|or|not|\(|\)(?:\s*\))*"
+func parseOr(tokens: var Deque[string]): Node
+func parseAtom(tokens: var Deque[string]): Node =
+  let token = tokens.popFirst
+  case token[0]:
+  of '#': result = Node(kind: nkTag, val: token[1..^1])
+  of '.': result = Node(kind: nkExtension, val: token[1..^1])
+  of 's': result = Node(kind: nkSysTag, val: token[2..^1])
+  of 't': result = Node(kind: nkType, val: token[2..^1])
+  of '(':
+    result = parseOr tokens
+    let nextTkn = tokens.peekFirst
+    if nextTkn == ")": tokens.popFirst
+    else: raise newException(ValueError, "Query parse Error: expected `)` but found `" & nextTkn & "`.")
+  else: raise newException(ValueError, "Query parse Error: expected tag atom but found an undefined token `" & token & "`.")
+
+func parseNot(tokens: var Deque[string]): Node =
+  if tokens.len > 0:
+    if tokens[0] == "not":
+      discard tokens.popFirst
+      result = Node(kind: nkNot, op: parseAtom tokens)
+    else: result = parseAtom tokens
+  else: raise newException(ValueError, "Query parse Error: expected `not` or tag atom but hit end of expression.")
+
+func parseAnd(tokens: var Deque[string]): Node =
+  result = parseNot tokens
+  while tokens.len > 0 and tokens.peekFirst == "and":
+    discard tokens.popFirst
+    result = Node(kind: nkAnd, leftOp: result, rightOp: parseNot tokens)
+
+func parseOr(tokens: var Deque[string]): Node =
+  result = parseAnd tokens
+  while tokens.len > 0 and tokens.peekFirst == "or":
+    discard tokens.popFirst
+    result = Node(kind: nkOr, leftOp: result, rightOp: parseAnd tokens)
+
+func tokenize(input: string): Deque[string] =
   const tokenRegx = re2"(s\/|#|\.)[a-zA-Z_]\w*|t\/[a-zA-Z]+|and|or|not|\(|\)"
-  echo input.match matchRgx
-  if input.match matchRgx:
-    input.findAll(tokenRegx).mapIt(input[it.boundaries])
-  else: raise newException(ValueError, "Invalid query!")
+  input.findAll(tokenRegx).mapIt(input[it.boundaries]).toDeque
 
-func parse(tokens: seq[string], operatorStack = newSeq[string](),
-           operandStack = newSeq[Node]()): Node =
-  # TODO: POPPING
-  # STACKING
-  # USE A STACK/DEQUEU AND TREAT EDGE CASES (STACKS EMPTINESS)
-  case tokens[0]:
-  of "not", "and", "or": parse(tokens[1..^1], tokens[0] & operatorStack, operandStack)
-  of "(": parse(tokens[1..^1], operatorStack, operandStack)
-  of ")": parse(tokens[1..^1], operatorStack, )
-  else:
-    let operand = case tokens[0][0]:
-      of '#': Node(kind: nkTag, val: tokens[0][1..^1])
-      of '.': Node(kind: nkExtension, val: tokens[0][1..^1])
-      of 's': Node(kind: nkSysTag, val: tokens[0][2..^1])
-      of 't': Node(kind: nkType, val: tokens[0][2..^1])
-      else: raise newException(ValueError, "Query not-valid. Expected operand but found '" & tokens[0] & "'")
-    if operatorStack.len == 0 or operatorStack[0] == "or":
-      parse(tokens[1..^1], operatorStack, operand & operandStack)
-    else:
-      if operatorStack[0] == "not":
-        parse(tokens[1..^1], operatorStack[1..^1],
-              Node(kind: nkNot, op: operand) & operandStack)
-      else:
-        parse(tokens[1..^1], operatorStack[1..^1],
-              Node(kind: nkAnd, leftOp: operandStack[0], rightOp: operand) & operandStack[1..^1])
+proc parse(input: string): Node =
+  var tokens = tokenize input
+  parseOr tokens
 
-proc find*(query: string) =
-  ## TODO
-  echo tokenize query
+proc find*(query: string) = echo repr parse query
 
-find "#bjg or (#E23_fg or ._F) or (not t/G and (t/F or .md and .sormek))"
+find "#tag1 and (.md) or (.txt and #tag2 or #tag3) and ((s/systag1 or t/img) and t/file)"
