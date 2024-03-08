8239decbd6bb4dc340848a7885251666aeef5b9a
Rami Kader <ramy.kader.rk@gmail.com>
2024-03-09 00:01:26 +0100

    feat - memos can be with no tags

--- src/app/memos.nim
+++ src/app/memos.nim
@@ -17,20 +17,15 @@ proc memo*(memoid: string) =
       if memoid.isEmptyOrWhitespace or memoid == "nil": newMemoFile()
       else: memoFile(memoid)
   
-  while tags.len == 0:
-    if execShellCmd(defaultEditor & " " & memoToOpen) == 0:
-      let content = readFile memoToOpen
-      tags = content
-        .findAll(re2"#[a-zA-Z_]\w*")
-        .mapIt(content[it.boundaries])
-        .toHashSet.toSeq
-    
-      if tags.len == 0:
-        echo """
-No tag was detected in edited memo!
-Make sure to add at least one #hashtag to ensure the memo's retrieval."""
-      # TODO: deal with file persistence in memos
-      # folder instead before adding tags
-      # also don't forget to delete invalidated tags
-      else: addTaggedFiles(@[memoToOpen], tags)
-    else: raise newException(IOError, "Something went wrong while trying to open/edit the memo!")
+  if execShellCmd(defaultEditor & " " & memoToOpen) == 0:
+    let content = readFile memoToOpen
+    tags = content
+      .findAll(re2"#[a-zA-Z_]\w*")
+      .mapIt(content[it.boundaries])
+      .toHashSet.toSeq
+  
+    # TODO: deal with file persistence in memos
+    # folder instead before adding tags
+    # also don't forget to delete invalidated tags
+    addTaggedFiles(@[memoToOpen], tags)
+  else: raise newException(IOError, "Something went wrong while trying to open/edit the memo!")
